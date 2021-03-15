02dca7e99399526bee1def4bf28ab07a0e80e118
Josh Graff <graffJ@jbu.edu>
2021-03-15 13:41:47 -0500

    Notes 3.15

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-if __name__ == "__main__":
-    import uvicorn
+from fastapi import FastAPI
 
-    uvicorn.run("app:app", reload=True)
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+    # class MedicineBottle(BaseModel):
+    #     bottle_name: str
+    #     drug_name: str
+    #     drug_dosage: str
+    #     list_of_times: list
+    #     drug_information: dict
+    #
+    # app = FastAPI()
+    #
+    #
+    # @app.get("/")
+    # async def root():
+    #     return {"message": "Hello World"}
+    #
+    #
+    # @app.post("/NewMedicineBottle/")
+    # async def create_medicine_bottle(new_med : MedicineBottle):
+    #     return new_med
+    #
+    # uvicorn.run("app:app", reload=True)
